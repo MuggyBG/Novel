ac5de9178ad17cdcf553ac434fc4b0ef65445730
MuggyBG <mugetsubg@gmail.com>
2026-04-06 22:07:13 +0300

    added temp router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Route, Routes, BrowserRouter} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/Contacts" element={<Contacts/>}/>
+        <Route path="*" element={<Home/>} />
+    </Routes>
+    </BrowserRouter>
+    
+    
   </StrictMode>,
 )
